b9625e08b831e81d2d40d49a780876f707d85be0
Bartosz <bartoszlis87@gmail.com>
2021-09-12 18:37:15 +0200

    add Task05 fixec chromedriver

--- src/main/java/Task05.java
+++ src/main/java/Task05.java
@@ -7,7 +7,7 @@ public class Task05 {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver",
-                "src/main/resources/drivers/chromedriver.exe");
+                "src/main/resources/drivers/chromedriver");
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
